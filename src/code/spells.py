84a4bfb5380893f38dfaa542e0125d3b5a581189
Aaron <aaron.c@thewagners.family>
2025-02-27 18:12:17 -0500

    U[date spell descriptions to fit on card

--- src/code/spells.py
+++ src/code/spells.py
@@ -3,17 +3,17 @@ from random import randint
 import random
 class src_spell(src_object):
     type=spell
-    damage_spell=spell(description='Garunteed to damage opponent; Uses 5 stamina')
-    heal_spell=spell('Healing Spell',condition={'target':'self','compare':'at_most','standard':'max_health'},description='Heal 2 to 12 HP; Uses 5 stamina')
-    shield_spell=spell('Shield Spell',10,1,condition=None,description='Heal 10 HP. Ignores max HP; Uses 10 stamina')
-    fire_spell=spell('Fire Spell',3,description='Two chances to damage opponent; Uses 3 stamina',element='fire')
-    sharpness_spell=spell('Sharpness Spell',15,2,condition=None,description='Increase damage by 1; Uses 15 stamina')
-    weakening_spell=spell('Weakening Spell',15,2,description='Decrease enemy damage by 1; Uses 15 stamina')
-    soul_tearing_spell=spell('Soul Tearing Spell',20,3,description='Decrease enemy\'s max HP by up to 6; Uses 20 stamina')
-    spell_of_wisdom=spell('Spell of Wisdom',15,2,condition=None,description='Increase skill by 0 to 2; Uses 15 stamina')
-    leech_spell=spell('Leech Spell',7,1,description='Chance to steal health; Uses 7 stamina')
+    damage_spell=spell(description=' Garunteed to\ndamage opponent\n Uses 5 stamina')
+    heal_spell=spell('Healing Spell',condition={'target':'self','compare':'at_most','standard':'max_health'},description='Heal 2 to 12 HP\n Uses 5 stamina')
+    shield_spell=spell('Shield Spell',10,1,condition=None,description='Heal 10 HP. Ignores max HP\n Uses 10 stamina')
+    fire_spell=spell('Fire Spell',3,description=' Two chances to\ndamage opponent\n Uses 3 stamina',element='fire')
+    sharpness_spell=spell('Sharpness Spell',15,2,condition=None,description='Increase damage by 1\n Uses 15 stamina')
+    weakening_spell=spell('Weakening Spell',15,2,description='Decrease enemy damage by 1\n Uses 15 stamina')
+    soul_tearing_spell=spell('Soul Tearing Spell',20,3,description=' Decrease enemy\'s max\nHP by up to 6\n Uses 20 stamina')
+    spell_of_wisdom=spell('Spell of Wisdom',15,2,condition=None,description='Increase skill by 0 to 2\n Uses 15 stamina')
+    leech_spell=spell('Leech Spell',7,1,description='Chance to steal health\n Uses 7 stamina')
     # summoning_spell=spell('Summoning Spell',15,2,'Increase Darkness by 1. When Darkness reaches 5, Xzaeon is summoned.\nUses 15 stamina')
-    tsunami_spell=spell('Tsunami Spell',10,1,'Unleash a wave of damage on an enemy\nDamage is a random number between 1 and 6\ntimes a random number between 1 and 3',element='water')
+    tsunami_spell=spell('Tsunami Spell',10,1,' Unleash a wave of damage\non an enemy\n Damage is a random number\n between 1 and 6\ntimes a random number\nbetween 1 and 3\n Uses 12 Stamina',element='water')
     magma_spell=spell('Magma Spell',12,1,'Five chances to damage enemy',element='fire')
     def tier(self,tier=0):
         return [spell for spell in self.all if spell.level==tier]
--- src/code/table_setup.py
+++ src/code/table_setup.py
@@ -12,11 +12,11 @@ class game(game):
         for i in range(player_count-1):
             i+=1
             current_player=self.players[(index+i)%player_count]
-            display(resize(create_card(display_texture=f'character/{current_player.texture}',text=current_player),0.75),pos=(710+i*350,290))
+            display(resize(create_card(display_texture=f'character/{current_player.texture}',text=current_player),0.75),pos=(2110-i*350,290))
         main_player=self.players[index]
         display(create_card(display_texture=f'character/{main_player.texture}',text=main_player,font_data=('Noto Sans',20)),pos=(240,640)) 
         item_x=550
-        for item in [src.action.attack,src.action.rest]: #Example deck for player
+        for item in main_player.deck: #Example deck for player
             display(resize(create_card(text=item),0.5),pos=(item_x,800))
             item_x+=200
         pg.display.flip()
@@ -26,6 +26,9 @@ main_game=game()
 if __name__ == '__main__':
     for _ in range(4):
         main_game.players.append(player(src.character.random()))
-    main_game.display()
-    print(len(main_game.players))
-    wait(5)
+    for chrctr in range(4):
+        for _ in range(3):
+            main_game.players[chrctr].deck.append(src.spell.random())
+    for x in range(4):
+        main_game.display(x)
+        wait(5)
